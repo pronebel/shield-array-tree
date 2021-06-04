d4bd868a1a831292032cf816c6b5e2d700f49e4c
tenero <nebel08@163.com>
2021-06-04 08:18:01 +0800

    1

--- src/main.js
+++ src/main.js
@@ -10,3 +10,59 @@ export default function howLongUntilLunch(hours, minutes) {
 	var millisecondsUntilLunchTime = millisecondsUntil(lunchtime(hours, minutes));
 	return ms(millisecondsUntilLunchTime, { long: true });
 }
+
+
+'use strict';
+
+const stepLarget = 10000
+
+
+var  tree = [{
+    name: 'root-1',
+    children: [{
+      name: 'root-1-1'
+    }, {
+      name: 'root-1-2',
+      children:[{
+        name: 'root-1-2-1'
+      }]
+    }]
+  },
+  {
+    name: 'book-1',
+    children: [ {
+      name: 'book-1-1',
+      children:[{
+        name: 'book-1-1-1'
+      }]
+    }]
+  }]
+
+function flattenTree(root, key,index=0,parentIndexes=[],parentDeeps=[]) {
+	let deep = root[key] && root[key].length > 0? root[key].length : 0
+	root.indexes = parentIndexes.slice()
+	root.indexes.push(index)
+	root.deeps= parentDeeps.slice()
+	if(deep>0){
+		
+		root.deeps.push(deep)
+	}
+  let flatten = [Object.assign({}, root)];
+  delete flatten[0][key];
+ 
+  
+
+  if (root[key] && root[key].length > 0) {
+	 
+    return flatten.concat(
+		root[key]
+			.map((child)=>flattenTree(child, key,index++,root.indexes,root.deeps))
+      		.reduce((a, b)=>a.concat(b), [])
+    );
+  }
+
+  return flatten;
+};
+
+
+let 
